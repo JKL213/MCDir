6500edaa0139e1b0c2994132b10ad690893d75cb
JKL213 <38791325+JKL213@users.noreply.github.com>
2019-06-20 20:09:09 +0200

    Alpha 1.0
    First version: Alpha 1.0!
    - Backup
    - Restore

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -26,12 +26,18 @@ Partial Class Form1
         Me.Label2 = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.cbNoOverwrite = New System.Windows.Forms.CheckBox()
+        Me.btnBackup = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.FolderBrowserDialog2 = New System.Windows.Forms.FolderBrowserDialog()
         Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog3 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
@@ -40,10 +46,10 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(22, 13)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(100, 31)
+        Me.Label1.Size = New System.Drawing.Size(79, 25)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Status:"
         '
@@ -51,7 +57,7 @@ Partial Class Form1
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(119, 19)
+        Me.Label2.Location = New System.Drawing.Point(102, 13)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(105, 25)
         Me.Label2.TabIndex = 1
@@ -59,95 +65,149 @@ Partial Class Form1
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(230, 12)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(306, 66)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(41, 95)
+        Me.PictureBox1.Size = New System.Drawing.Size(76, 95)
         Me.PictureBox1.TabIndex = 2
         Me.PictureBox1.TabStop = False
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.Button2)
-        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Controls.Add(Me.cbNoOverwrite)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.btnBackup)
         Me.GroupBox1.Location = New System.Drawing.Point(19, 47)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(200, 60)
+        Me.GroupBox1.Size = New System.Drawing.Size(136, 124)
         Me.GroupBox1.TabIndex = 3
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Backup"
+        Me.GroupBox1.Text = "Create backup"
         '
-        'Button2
+        'RadioButton2
         '
-        Me.Button2.Location = New System.Drawing.Point(89, 20)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Load"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(6, 74)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(55, 17)
+        Me.RadioButton2.TabIndex = 7
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Saves" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.RadioButton2.UseVisualStyleBackColor = True
         '
-        'Button1
+        'RadioButton1
         '
-        Me.Button1.Location = New System.Drawing.Point(7, 20)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Create Backup"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(6, 51)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(51, 17)
+        Me.RadioButton1.TabIndex = 6
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Mods" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.RadioButton1.UseVisualStyleBackColor = True
         '
-        'GroupBox2
+        'cbNoOverwrite
         '
-        Me.GroupBox2.Controls.Add(Me.Button5)
-        Me.GroupBox2.Controls.Add(Me.Button4)
-        Me.GroupBox2.Controls.Add(Me.Button3)
-        Me.GroupBox2.Location = New System.Drawing.Point(19, 113)
-        Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(252, 81)
-        Me.GroupBox2.TabIndex = 4
-        Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "Mod Storage"
+        Me.cbNoOverwrite.AutoSize = True
+        Me.cbNoOverwrite.Location = New System.Drawing.Point(7, 97)
+        Me.cbNoOverwrite.Name = "cbNoOverwrite"
+        Me.cbNoOverwrite.Size = New System.Drawing.Size(125, 17)
+        Me.cbNoOverwrite.TabIndex = 3
+        Me.cbNoOverwrite.Text = "Do not overwrite files"
+        Me.cbNoOverwrite.UseVisualStyleBackColor = True
+        '
+        'btnBackup
+        '
+        Me.btnBackup.Location = New System.Drawing.Point(7, 20)
+        Me.btnBackup.Name = "btnBackup"
+        Me.btnBackup.Size = New System.Drawing.Size(75, 23)
+        Me.btnBackup.TabIndex = 0
+        Me.btnBackup.Text = "Create Backup"
+        Me.btnBackup.UseVisualStyleBackColor = True
+        '
+        'FolderBrowserDialog1
+        '
+        Me.FolderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.ApplicationData
+        '
+        'FolderBrowserDialog2
+        '
+        Me.FolderBrowserDialog2.SelectedPath = "C:\MCDir\Backups"
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(7, 20)
+        Me.Button3.Location = New System.Drawing.Point(27, 195)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 0
-        Me.Button3.Text = "Verify"
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "CF"
         Me.Button3.UseVisualStyleBackColor = True
         '
-        'Button4
+        'GroupBox2
         '
-        Me.Button4.Location = New System.Drawing.Point(7, 50)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(75, 23)
-        Me.Button4.TabIndex = 1
-        Me.Button4.Text = "Load config"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Controls.Add(Me.RadioButton3)
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Location = New System.Drawing.Point(162, 47)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(138, 124)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Restore backup"
         '
-        'Button5
+        'Button1
         '
-        Me.Button5.Location = New System.Drawing.Point(105, 20)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(75, 23)
-        Me.Button5.TabIndex = 2
-        Me.Button5.Text = "Save config"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.Button1.Location = New System.Drawing.Point(6, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Restore"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'FolderBrowserDialog3
+        '
+        Me.FolderBrowserDialog3.SelectedPath = "C:\MCDir\Backups"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(7, 74)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(51, 17)
+        Me.RadioButton3.TabIndex = 1
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Mods" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(7, 96)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(55, 17)
+        Me.RadioButton4.TabIndex = 2
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Saves"
+        Me.RadioButton4.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(382, 290)
+        Me.ClientSize = New System.Drawing.Size(414, 237)
         Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "MCDir (Early Alpha)"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -156,11 +216,17 @@ Partial Class Form1
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
-    Friend WithEvents Button2 As System.Windows.Forms.Button
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
-    Friend WithEvents Button5 As System.Windows.Forms.Button
-    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents btnBackup As System.Windows.Forms.Button
+    Friend WithEvents cbNoOverwrite As System.Windows.Forms.CheckBox
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents FolderBrowserDialog2 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
     Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents FolderBrowserDialog3 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,190 @@
-﻿Public Class Form1
+﻿Option Strict On
+Imports System.IO
+Public Class Form1
+    Dim BackupType As String
+    Dim FolderDirectory As New Hashtable
+    Dim MainFolder As String
+    Dim lstAllFiles As New List(Of String)
+    Dim lstDirectories As New List(Of String)
+    Dim CountMarker As Integer = 0
 
+    Private Sub btnBackup_Click(sender As Object, e As EventArgs) Handles btnBackup.Click
+        If RadioButton1.Checked = True Then
+            BackupType = "mods"
+            PictureBox1.BackgroundImage = My.Resources.tly
+            PictureBox1.Update()
+            MainFolder = Environ("AppData") & "\.minecraft\mods\"
+            Label2.Text = "Backup running..."
+            Label2.Refresh()
+        ElseIf RadioButton2.Checked = True Then
+            PictureBox1.BackgroundImage = My.Resources.tly
+            PictureBox1.Update()
+            BackupType = "saves"
+            MainFolder = Environ("AppData") & "\.minecraft\saves"
+            Label2.Text = "Backup running..."
+            Label2.Refresh()
+        End If
+        Dim fileDateTime As String = DateTime.Now.ToString("yyyyMMdd") & "_" & DateTime.Now.ToString("HHmmss")
+        Dim CurrentDir As String
+        CurrentDir = "C:\MCDir\Backups\" & fileDateTime & BackupType
+
+        My.Computer.FileSystem.CreateDirectory(CurrentDir)
+
+        Threading.Thread.Sleep(2000)
+        Label2.Text = "Backup running..."
+        Threading.Thread.Sleep(2000)
+        Dim count As Integer = 0
+        FolderDirectory.Add(count, CurrentDir)
+        Label2.Text = "Backup running..."
+        lstAllFiles.AddRange(Directory.GetFiles(MainFolder))
+        CopyFiles(count)
+
+
+        lstDirectories.AddRange(Directory.GetDirectories(MainFolder))
+
+        Dim FolderMarker As Integer = 0
+        For Each folder In lstDirectories
+            CopyDirectories(folder, count, FolderMarker)
+        Next
+
+        lstAllFiles.Clear()
+        lstDirectories.Clear()
+        FolderDirectory.Clear()
+        count = 0
+        CountMarker = 0
+        cbNoOverwrite.Checked = False
+        Label2.Text = "Idle"
+        PictureBox1.BackgroundImage = My.Resources.tlg
+        MessageBox.Show("All files are successfully backed up!", "Completed!")
+
+
+
+    End Sub
+
+#Region "Methods"
+    ''' <summary>
+    ''' This is a method that copies files in the chosen directory into the backup location
+    ''' </summary>
+    ''' <param name="count">This parameter is used to pick the correct folder directory when copying files over</param>
+    ''' <remarks></remarks>
+    Sub CopyFiles(ByVal count As Integer)
+        Try
+            For Each File In lstAllFiles
+                Dim FileInfo As New FileInfo(File)
+                Dim Filename As String = FileInfo.Name
+                Dim FileLocation As String = FileInfo.FullName
+                Dim FileDate As Date = FileInfo.LastWriteTime
+
+                If System.IO.File.Exists(Path.Combine(CStr(FolderDirectory(count)), Filename)) Then
+                    Dim TempFileinfo As New FileInfo(Path.Combine(CStr(FolderDirectory(count)), Filename))
+                    Dim TempFileDate As Date = TempFileinfo.LastWriteTime
+                    Dim result As Integer = DateTime.Compare(FileDate, TempFileDate)
+
+                    If result > 0 AndAlso cbNoOverwrite.Checked = False Then
+                        System.IO.File.Copy(FileLocation, Path.Combine(CStr(FolderDirectory(count)), Filename), True)
+                    End If
+                Else
+                    System.IO.File.Copy(FileLocation, Path.Combine(CStr(FolderDirectory(count)), Filename))
+                End If
+            Next
+        Catch ex As Exception
+            PictureBox1.BackgroundImage = My.Resources.tlr
+            MessageBox.Show("Oops! Something went wrong! " & ex.ToString, "DTA stream error ")
+        End Try
+    End Sub
+    ''' <summary>
+    ''' This sub copies folder directories over to the backup location and uses recursion to call itself so that it looks at directories in itself and also calls CopyFiles to copy the files over in those directories
+    ''' </summary>
+    ''' <param name="Folder">This parameter holds the folder location</param>
+    ''' <param name="count">This parameter holds the count used to add more directories into the hashtable and used so that it creates a unique ID for each folder</param>
+    ''' <param name="FolderMarker">this parameter is used a marker so that it adds the correct folder directory path</param>
+    ''' <remarks></remarks>
+    Sub CopyDirectories(ByVal Folder As String, ByVal count As Integer, ByVal FolderMarker As Integer)
+        Try
+            'Check if folder exists in backup and if it doesnt copy entire folder over
+            'if folder exists then go through each folder and copy files/folders over
+            'use recursion to call 
+            Dim FolderInfo As New DirectoryInfo(Folder)
+            Dim Folderlocation As String = FolderInfo.FullName
+            Dim FolderName As String = FolderInfo.Name
+            Dim lstTempFolderDirectories As New List(Of String)
+            lstTempFolderDirectories.AddRange(Directory.GetDirectories(Folderlocation))
+
+            'if the folder does not exist then copy folder and contents over otherwise read through contents and update if necassary
+            If Directory.Exists(Path.Combine(CStr(FolderDirectory(count)), FolderName)) Then
+                lstAllFiles.Clear()
+                lstAllFiles.AddRange(Directory.GetFiles(Folderlocation))
+
+                'Loop until the count is higher than the countmarker so that the count is a new number and that the FolderDirectory doesnt try to add a directory in a used key
+                Do Until count > CountMarker
+                    count += 1
+                Loop
+                CountMarker = count
+
+                'Add directory to hashtable
+                FolderDirectory.Add((count), (Path.Combine(CStr(FolderDirectory(FolderMarker)), FolderName)))
+
+                'Add 1 to the foldermarker here
+                FolderMarker += 1
+
+                'Copy files in this directory over
+                CopyFiles(count)
+
+                If lstTempFolderDirectories.Contains(Nothing) Then
+                Else
+                    'Use recursion to call this same subroutine so that it can go through each subfolders
+                    For Each Folder In lstTempFolderDirectories
+                        CopyDirectories(Folder, count, FolderMarker)
+                    Next
+                End If
+            Else
+                'if folder does not exist then copy over whole of the directory including everything inside it
+                My.Computer.FileSystem.CopyDirectory(Folderlocation, Path.Combine(CStr(FolderDirectory(FolderMarker)), FolderName))
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Something went wrong. " & ex.ToString, "Data error ")
+        End Try
+    End Sub
+#End Region
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        FolderBrowserDialog1.ShowDialog()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        FolderBrowserDialog2.ShowDialog()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label2.Text = "Idle"
+        MainFolder = "%AppData%\.minecraft\"
+        PictureBox1.BackgroundImage = My.Resources.tlg
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs)
+        FolderBrowserDialog1.ShowDialog()
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        My.Computer.FileSystem.CreateDirectory(
+            "C:\MCDir")
+        My.Computer.FileSystem.CreateDirectory(
+            "C:\MCDir\Backups")
+    End Sub
+
+    Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles Button1.Click
+        FolderBrowserDialog3.ShowDialog()
+        My.Computer.FileSystem.CopyDirectory(FolderBrowserDialog3.SelectedPath, Environ("AppData") & "\.minecraft\mods\")
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+
+    End Sub
 End Class
